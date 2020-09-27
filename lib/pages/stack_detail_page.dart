4064a069527fbd9a08c2373d56a4cf770ed7310b
Joel <joel.rose@gmx.net>
2020-09-27 11:56:11 +0200

    Create Stack Detail page

---
+++ lib/pages/stack_detail_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:src/models/Charity.dart';
+
+
+class SlackDetailPage extends StatefulWidget {
+  final Charity charity;
+
+  SlackDetailPage({@required this.charity});
+
+  @override
+  _SlackDetailPageState createState() => _SlackDetailPageState();
+}
+
+class _SlackDetailPageState extends State<SlackDetailPage> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+  }
+}
